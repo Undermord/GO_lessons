3bf0e5f1f1060d6960dff996f0808912313017a1
Vladislav A. Gusev <v.gusev@hserm.ru>
2024-11-20 02:15:09 +0400

    добавил функционал для других страниц и сделал новый путь с проверкой на метод для формы

--- main.go
+++ main.go
@@ -5,11 +5,32 @@ import (
 	"net/http"
 )
 
-func main () {
+func main() {
+	// Обработчик для корня
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Приветствую")
-		})
+	})
+	// Обработчик для другой страницы
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Это простой пет-проект на Go")
 
-fmt.Println("Сервер запущен на порте 8080")
-http.ListenAndServe(":8080", nil)
+	})
+
+	// Для статистических файлов
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	// Для формы
+	http.HandleFunc("/submit", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Метод не поддерживается", http.StatusMethodNotAllowed)
+			return
+		}
+		name := r.FormValue("name")
+		email := r.FormValue("email")
+
+		fmt.Fprintf(w, "Имя: %s\nEmail: %s", name, email)
+	})
+
+	fmt.Println("Сервер запущен на порте 8080")
+	http.ListenAndServe(":8080", nil)
 }
